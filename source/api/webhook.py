4cfa1ed00526446c083098d5010a792f18379804
kanaarima <137962405+kanaarima@users.noreply.github.com>
2023-07-14 15:51:44 +0200

    Transition to local webhooks via flask2discord

--- source/api/webhook.py
+++ source/api/webhook.py
@@ -1,11 +1,11 @@
-from discord_webhook import DiscordWebhook, DiscordEmbed
+import requests
 import time
 
-def send_content_raw(url, content):
-    webhook = DiscordWebhook(url=url, rate_limit_retry=True, content=content)
-    webhook.execute()
+#def send_content_raw(url, content):
+#    webhook = DiscordWebhook(url=url, rate_limit_retry=True, content=content)
+#    webhook.execute()
 
-def send_string_list(url, title, content):
+def send_string_list(url, channel_id, title, content):
     strings = list()
     string = f"```{title}\n"
     for str in content:
@@ -17,6 +17,5 @@ def send_string_list(url, title, content):
     string += "```"
     strings.append(string)
     for str in strings:
-        webhook = DiscordWebhook(url=url, rate_limit_retry=True, content=str)
-        time.sleep(0.5)
-        webhook.execute()
+        requests.post(url, json={"channel_id": channel_id, "message": str}, headers={'Content-Type': 'application/json'})
+        time.sleep(1)
--- source/trackers/clan_lb_tracker.py
+++ source/trackers/clan_lb_tracker.py
@@ -9,6 +9,67 @@ import unicodedata
 
 thread = None
 
+def get_data_pp():
+    data = dict()
+    data["std_vn_pp"] = akat.grab_clan_ranking(mode=0, relax=0, pp=True)
+    data["std_rx_pp"] = akat.grab_clan_ranking(mode=0, relax=1, pp=True)
+    data["std_ap_pp"] = akat.grab_clan_ranking(mode=0, relax=2, pp=True)
+    data["taiko_vn_pp"] = akat.grab_clan_ranking(mode=1, relax=0, pp=True)
+    data["taiko_rx_pp"] = akat.grab_clan_ranking(mode=1, relax=1, pp=True)
+    data["ctb_vn_pp"] = akat.grab_clan_ranking(mode=2, relax=0, pp=True)
+    data["ctb_rx_pp"] = akat.grab_clan_ranking(mode=2, relax=1, pp=True)
+    data["mania_vn_pp"] = akat.grab_clan_ranking(mode=3, relax=0, pp=True)
+    return data
+
+def get_data_1s():
+    data = dict()
+    data["std_vn_1s"] = akat.grab_clan_ranking(mode=0, relax=0)
+    data["std_rx_1s"] = akat.grab_clan_ranking(mode=0, relax=1)
+    data["std_ap_1s"] = akat.grab_clan_ranking(mode=0, relax=2)
+    data["taiko_vn_1s"] = akat.grab_clan_ranking(mode=1, relax=0)
+    data["taiko_rx_1s"] = akat.grab_clan_ranking(mode=1, relax=1)
+    data["ctb_vn_1s"] = akat.grab_clan_ranking(mode=2, relax=0)
+    data["ctb_rx_1s"] = akat.grab_clan_ranking(mode=2, relax=1)
+    data["mania_vn_1s"] = akat.grab_clan_ranking(mode=3, relax=0)
+    data["overall_1s"] = {"clans": get_overall_1s(data)}
+    return data
+
+def get_overall_1s(data):
+    res = list()
+    clans = dict()
+    clans_metadata=dict()
+    for key in data.keys():
+        for clan in data[key]["clans"]:
+            if clan["clan"] in clans:
+                clans[clan["clan"]] += clan["count"]
+            else:
+                clans[clan["clan"]] = clan["count"]
+                clans_metadata[clan["clan"]] = clan.copy()
+    sortedclans = dict(sorted(clans.items(), key=lambda x:x[1], reverse=True))
+    for k,v in sortedclans.items():
+        clan = clans_metadata[k].copy()
+        clan["count"] = v
+        res.append(clan)
+    return res
+
+def format_1s_string(clan, rank, rank_gain, count_gain):
+    if rank_gain < 0:
+        gain_str = f"({rank_gain})"
+    elif rank_gain > 0:
+        gain_str = f"(+{rank_gain})"
+    else:
+        gain_str = ""
+    if count_gain < 0:
+        gain_str2 = f"({count_gain})"
+    elif count_gain > 0:
+        gain_str2 = f"(+{count_gain})"
+    else:
+        gain_str2 = ""
+    type_str = "First places"
+    clan_tag = unicodedata.normalize("NFC", clan["tag"])
+    clan_name = unicodedata.normalize("NFC", clan["name"])
+    return f"#{rank} {clan_name} [{clan_tag}] {gain_str} {type_str}: {clan['count']} {gain_str2}"
+
 def track_clan_leaderboards(secrets):
     while True:
         thread.sleeping = False
@@ -24,46 +85,11 @@ def track_clan_leaderboards(secrets):
             with open(f"data/clan_lb/{yesterday}.json") as f:
                 olddata = json.load(f)
         os.makedirs("data/clan_lb/", exist_ok=True)
-        data = dict()
-        data["std_vn_1s"] = akat.grab_clan_ranking(mode=0, relax=0)
-        data["std_rx_1s"] = akat.grab_clan_ranking(mode=0, relax=1)
-        data["std_ap_1s"] = akat.grab_clan_ranking(mode=0, relax=2)
-        data["taiko_vn_1s"] = akat.grab_clan_ranking(mode=1, relax=0)
-        data["taiko_rx_1s"] = akat.grab_clan_ranking(mode=1, relax=1)
-        data["ctb_vn_1s"] = akat.grab_clan_ranking(mode=2, relax=0)
-        data["ctb_rx_1s"] = akat.grab_clan_ranking(mode=2, relax=1)
-        data["mania_vn_1s"] = akat.grab_clan_ranking(mode=3, relax=0)
-        #data["mania_rx_1s"] = akat.grab_clan_ranking(mode=3, relax=1)
-        #data["std_vn_pp"] = akat.grab_clan_ranking(mode=0, relax=0, pp=True)
-        #data["std_rx_pp"] = akat.grab_clan_ranking(mode=0, relax=1, pp=True)
-        #data["std_ap_pp"] = akat.grab_clan_ranking(mode=0, relax=2, pp=True)
-        #data["taiko_vn_pp"] = akat.grab_clan_ranking(mode=1, relax=0, pp=True)
-        #data["taiko_rx_pp"] = akat.grab_clan_ranking(mode=1, relax=1, pp=True)
-        #data["ctb_vn_pp"] = akat.grab_clan_ranking(mode=2, relax=0, pp=True)
-        #data["ctb_rx_pp"] = akat.grab_clan_ranking(mode=2, relax=1, pp=True)
-        #data["mania_vn_pp"] = akat.grab_clan_ranking(mode=3, relax=0, pp=True)
-        #data["mania_rx_pp"] = akat.grab_clan_ranking(mode=3, relax=1, pp=True)
-        data["overall_1s"] = {"clans": list()}
-        clans = dict()
-        clans_metadata=dict()
-        for key in data.keys():
-            if not key.endswith("1s"):
-                continue
-            for clan in data[key]["clans"]:
-                if clan["clan"] in clans:
-                    clans[clan["clan"]] += clan["count"]
-                else:
-                    clans[clan["clan"]] = clan["count"]
-                    clans_metadata[clan["clan"]] = clan
-        sortedclans = dict(sorted(clans.items(), key=lambda x:x[1], reverse=True))
-        for k,v in sortedclans.items():
-            clan = clans_metadata[k].copy()
-            clan["count"] = v
-            data["overall_1s"]["clans"].append(clan)
+        data = get_data_1s()
         with open(filename, "w") as f:
             json.dump(data, f, indent=4)
         for key in data.keys():
-            l = list()
+            clan_list = list()
             rank = 0
             if not data[key]["clans"]:
                 continue
@@ -78,28 +104,7 @@ def track_clan_leaderboards(secrets):
                         if clanold["clan"] == clan["clan"]:
                             count_gain = clan["count"]-clanold["count"]
                             rank_gain = rankold-rank
-                if rank_gain < 0:
-                    gain_str = f"({rank_gain})"
-                elif rank_gain > 0:
-                    gain_str = f"(+{rank_gain})"
-                else:
-                    gain_str = ""
-                if count_gain < 0:
-                    gain_str2 = f"({count_gain})"
-                elif count_gain > 0:
-                    gain_str2 = f"(+{count_gain})"
-                else:
-                    gain_str2 = ""
-                type_str = "First places" if key.endswith("1s") else "Performance Points"
-                if "tag" in clan:
-                    clan_tag = unicodedata.normalize("NFC", clan["tag"])
-                else:
-                    continue
-                if "name" in clan:
-                    clan_name = unicodedata.normalize("NFC", clan["name"])
-                else:
-                    continue
-                if "count" not in clan:
-                    continue
-                l.append(f"#{rank} {clan_name} [{clan_tag}] {gain_str} {type_str}: {clan['count']} {gain_str2}")
-            wh.send_string_list(secrets[key], f"{today} changes", l)
+                clan_list.append(format_1s_string(clan, rank, rank_gain, count_gain))    
+            if key in secrets:
+                wh.send_string_list(secrets["flask2discord"], secrets[key], f"{today} changes", clan_list)
+
