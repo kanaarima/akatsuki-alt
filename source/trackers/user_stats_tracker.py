f6b90dfe8ead96c7ebf5687e80a9bdef60c952c8
kanaarima <137962405+kanaarima@users.noreply.github.com>
2023-07-23 14:02:16 +0200

    Add exception handling

--- source/trackers/user_stats_tracker.py
+++ source/trackers/user_stats_tracker.py
@@ -1,9 +1,21 @@
+import api.webhook as wh
+import traceback
 import json
 import time
+import glob
 
 thread = None
 
 
 def user_stats_tracker(secrets):
     while True:
-        time.sleep(1)
+        try:
+            time.sleep(1)
+        except Exception as e:
+            print(e)
+            if "error_channel" in secrets:
+                wh.send_string_list(
+                    f'http://{secrets["flask2discord_host"]}:{secrets["flask2discord_port"]}/send_message',
+                    secrets["error_channel"], f"Error in {__name__}",
+                    (e.__class__.__name__, str(e), traceback.format_exc()))
+            time.sleep(10)
