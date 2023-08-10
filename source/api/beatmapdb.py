8a3420ee9218f2941bbb1a189b84c329a5aff76d
kanaarima <137962405+kanaarima@users.noreply.github.com>
2023-08-11 01:33:59 +0200

    Support fetching missing beatmap data from bancho

--- source/api/beatmapdb.py
+++ source/api/beatmapdb.py
@@ -1,4 +1,6 @@
 import api.utils as utils
+import api.bancho as bancho
+import time
 import csv
 import os
 
@@ -25,19 +27,30 @@ def _blank_beatmap():
         "beatmap_id": 0,
         "beatmap_set_id": 0,
         "stars": 0,
+        "stars_aim": 0,
+        "stars_speed": 0,
         "artist": "",
         "title": "",
         "difficulty": "",
         "source": "",
+        "CS": 0,
+        "OD": 0,
+        "AR": 0,
+        "HP": 0,
+        "length": 0,
+        "length_drain": 0,
     }
 
 
-def update_beatmaps(data: list, maps: list):
+def update_beatmaps(data: list, maps: list, overwrite=False):
     table = dict()
     for beatmap in data:
         table[beatmap["beatmap_id"]] = beatmap
     for beatmap in maps:
         if beatmap["beatmap_id"] not in table:
+            data.append(beatmap)
+        elif overwrite:
+            data.remove(table[beatmap["beatmap_id"]])
             data.append(beatmap)
 
 
@@ -49,6 +62,14 @@ def convert_osualt_csv_str(values):
     map["beatmap_set_id"] = int(values[6])
     map["artist"] = values[5]
     map["stars"] = float(values[10])
+    map["stars_aim"] = float(values[11])
+    map["stars_speed"] = float(values[12])
+    map["CS"] = float(values[13])
+    map["OD"] = float(values[14])
+    map["AR"] = float(values[15])
+    map["HP"] = float(values[16])
+    map["length"] = float(values[22])
+    map["length_drain"] = float(values[17])
     map["title"] = values[21]
     map["difficulty"] = values[23]
     return map
@@ -76,5 +97,41 @@ def load_osualt_csv():
             if values[0] == "beatmap_id":  # description string
                 continue
             csv_maps.append(convert_osualt_csv_str(values))
-    update_beatmaps(beatmaps, csv_maps)
+    update_beatmaps(beatmaps, csv_maps, overwrite=True)
     save_beatmaps(beatmaps)
+
+
+def repair_maps(data):
+    for map in data:
+        repair = False
+        if map["stars"] == 0:
+            repair = True
+        if "OD" not in map:
+            repair = True
+        if not repair:
+            continue
+        empty = _blank_beatmap()
+        for key in empty.keys():
+            if key not in map:
+                map[key] = empty[key]
+        try:
+            apimap = bancho.client.beatmap(beatmap_id=map["beatmap_id"])
+            attr = bancho.client.beatmap_attributes(
+                beatmap_id=map["beatmap_id"]
+            ).attributes
+            time.sleep(2)
+        except Exception as e:
+            print(e)
+            time.sleep(1)
+            continue
+        map["artist"] = apimap._beatmapset.artist
+        map["title"] = apimap._beatmapset.title
+        map["difficulty"] = apimap.version
+        map["stars"] = attr.star_rating
+        map["stars_speed"] = attr.aim_difficulty
+        map["stars_aim"] = attr.speed_difficulty
+        map["CS"] = apimap.cs
+        map["OD"] = attr.overall_difficulty
+        map["AR"] = attr.approach_rate
+        map["length"] = apimap.total_length
+        map["length_drain"] = apimap.drain
